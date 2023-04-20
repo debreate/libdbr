549aeb03f1df22bfed1bd444718a96aa030dd857
Jordan Irwin <antumdeluge@gmail.com>
2023-04-19 22:13:49 -0700

    Fix returning default value of Config.getValue when section not found

--- lib/libdbr/config.py
+++ lib/libdbr/config.py
@@ -240,6 +240,8 @@ class Config:
   def getValue(self, key, default=None, section=None):
     section = section or self.__default_section
     if section not in self.__sections:
+      if default != None:
+        return default
       raise KeyError("section '{}' not found in config '{}'".format(section, self.__filepath))
 
     s_data = self.__sections[section]
